cc665a1333b9f09f918695ae38cd4646f8deb42a
Rishabh Verma <rishabhverma2503@gmail.com>
2024-02-24 18:16:12 -0800

    working but wont show or update details

--- src/main/java/ca/sfu/cmpt276/as2/studenttrack/controllers/StudentController.java
+++ src/main/java/ca/sfu/cmpt276/as2/studenttrack/controllers/StudentController.java
@@ -1,15 +1,20 @@
 package ca.sfu.cmpt276.as2.studenttrack.controllers;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import ca.sfu.cmpt276.as2.studenttrack.models.Student;
 import ca.sfu.cmpt276.as2.studenttrack.models.StudentRepo;
@@ -28,7 +33,7 @@ public class StudentController {
     }
 
     @PostMapping("/student/add")
-    public String addUser(@RequestParam Map<String, String> newStudent, HttpServletResponse response){
+    public String addStudent(@RequestParam Map<String, String> newStudent, HttpServletResponse response){
         System.out.println("ADD user");
         String newName = newStudent.get("name");
         int newWeight = Integer.parseInt(newStudent.get("weight"));
@@ -38,5 +43,52 @@ public class StudentController {
         studentRepo.save(new Student(newName, newWeight, newHeight, newHairColor, newGPA));
         response.setStatus(201);
         return "student/addedStudent";
+    }
+
+    @GetMapping("/student/{id}")
+    public ResponseEntity<Student> getStudentById(@PathVariable int id) {
+        Student student = studentRepo.findById(id);
+        if (student != null) {
+            return new ResponseEntity<>(student, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PostMapping("/student/update")
+    public ResponseEntity<String> updateStudent(@RequestParam("id") int id, @RequestBody Map<String, String> updatedStudentInfo) {
+        // Find the student by id
+        Student student = studentRepo.findById(id);
+        if (student == null) {
+            return new ResponseEntity<>("Student not found", HttpStatus.NOT_FOUND);
+        }
+
+        // Update student information
+        if (updatedStudentInfo.containsKey("name")) {
+            student.setName(updatedStudentInfo.get("name"));
+        }
+        if (updatedStudentInfo.containsKey("weight")) {
+            student.setWeight(Integer.parseInt(updatedStudentInfo.get("weight")));
+        }
+        if (updatedStudentInfo.containsKey("height")) {
+            student.setHeight(Integer.parseInt(updatedStudentInfo.get("height")));
+        }
+        if (updatedStudentInfo.containsKey("hairColor")) {
+            student.setHairColor(updatedStudentInfo.get("hairColor"));
+        }
+        if (updatedStudentInfo.containsKey("gpa")) {
+            student.setGpa(Double.parseDouble(updatedStudentInfo.get("gpa")));
+        }
+
+        // Save the updated student
+        studentRepo.save(student);
+
+        return new ResponseEntity<>("Student updated successfully", HttpStatus.OK);
+    }
+
+    @DeleteMapping("/student/delete")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteStudentByID(@RequestParam int sid) {
+        studentRepo.deleteAllById(sid);
     }
 }
